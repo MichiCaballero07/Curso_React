32efb26b2ad226884ad40e504d681d2912a9c3d2
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-08-22 15:22:46 -0500

    new file

---
+++ js/01.js
@@ -0,0 +1,12 @@
+// Variables 
+// 1. var: ya casi no se utiliza en la sintaxis moderna
+// 2. let
+// 3. const javascript es de tipo dinamico el tip de dato se almacena en el valor de la variable y no en la variable como tal
+// creas una variable como tipo string y despues le puedes asignar otros tipos de datos 
+let client = "Juan";
+let precioDescuento = 220;
+
+console.log(client);
+console.log(precioDescuento);
+
+
