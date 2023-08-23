dd4f0b56d9e9503599c868882a309d736a586e27
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-08-23 16:09:50 -0500

    new file

--- js/01.js
+++ js/01.js
@@ -4,9 +4,18 @@
 // 3. const javascript es de tipo dinamico el tip de dato se almacena en el valor de la variable y no en la variable como tal
 // creas una variable como tipo string y despues le puedes asignar otros tipos de datos 
 let client = "Juan";
-let precioDescuento = 220;
+let precioDescuento = 220; // CamelCase es la forma en la que se escriben las variables o funciones que tiene mas de una palabra
 
+
+// Reasignar
+client = 20
+client = true
+
+// Pueden iniciar sin un valor
+let precio 
+precio = 1000;
+
+console.log(precio);
 console.log(client);
 console.log(precioDescuento);
-
 
