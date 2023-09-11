8b76648eafb6fe13ac381f1a61b3a50ebd48388d
MichiCaballeo07 <michelcaballerogranado@gmail.com>
2023-09-11 15:23:43 -0500

    add new file

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -1,3 +1,8 @@
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
+
+
 //JSX JavaScript Syntax Extension - es una extensión desarrolada por Facebook para React
 // básicamente es un lenguaje de Template que muestra HTML pero tiene todas las funciones de JavaScript
 // se va a dividir en dos partes en lo que se pone dentro de la funcion y lo que se pone en el return
@@ -8,7 +13,9 @@ function App() {
 
   return (
     <>
-
+      <Header/>
+      <Formulario/>
+      <ListadoPacientes/>
     </>
   )
 }
--- citas-react/src/components/Header.jsx
+++ citas-react/src/components/Header.jsx
@@ -1,16 +1,13 @@
-import Header from "./com"
-
 // Los componentes deben de ser nombrados con la primera letra en mayuscula
 // Un componente es una función 
 
 function Header() {
 
+    //para crear una función o una validación
 
     return (
-        <>
-
-        </>
+        <h1>Desde Header</h1>
     )
 }
 
-export default Header
+export default Header;
